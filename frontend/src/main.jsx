679f67489f2e8dda28ae0a37a77fea48a9690842
Nitesh Gupta <niteshgupta2026@gmail.com>
2025-09-08 14:27:10 +0530

    api url fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: API_URL, // backend URL
 });
 
 // Add token to requests if exists
